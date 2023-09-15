4b2b170da9bc54ef01784aa663f145c7ed9cae23
thejavidev <javidation.l@gmail.com>
2023-09-15 17:43:40 +0400

    v2.3

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import { shoes1, shoes2, shoes3, shoes4 } from "./assets";
 import { BiPlusMedical } from "react-icons/bi";
 import { FaMinus } from "react-icons/fa";
 import { SlBasket } from "react-icons/sl";
+import { basket } from "./data/fakeData";
 const Home = () => {
   const bigSrc = useRef();
   const smallSrc = useRef();
@@ -25,92 +26,75 @@ const Home = () => {
   return (
     <>
       <div className="mt-[8.125rem] "></div>
-      <div className="grid grid-cols-2 gap-4 px-[9.375rem] xl:px-[3.125rem] lg:px-[1.25rem] md:grid-cols-1">
-        <div className=" px-[5rem] md:px-[0rem] py-[1.25rem] lg:px-[1.25rem]">
-          <img
-            ref={bigSrc}
-            className="h-[28.125rem] md:h-[25rem] w-full object-cover rounded-xl mb-[1.25rem]"
-            src={bigImgSrc ? bigImgSrc : shoes1}
-            alt=""
-          />
-          <div className="grid grid-cols-4 gap-4 images">
-            <div className="picture rounded-xl relative">
-              <img
-                onClick={handleSmallImageClick}
-                ref={smallSrc}
-                src={shoes2}
-                className="rounded-xl h-[6.25rem] md:h-[5rem] w-full object-cover cursor-pointer relative  "
-                alt=""
-              />
-            </div>
-            <div className="picture  rounded-xl relative">
-              <img
-                onClick={handleSmallImageClick}
-                src={shoes1}
-                className="rounded-xl h-[6.25rem] md:h-[5rem] w-full object-cover cursor-pointer relative  "
-                alt=""
-              />
-            </div>
-            <div className="picture  rounded-xl relative">
-              <img
-                onClick={handleSmallImageClick}
-                src={shoes3}
-                className="rounded-xl h-[6.25rem] md:h-[5rem] w-full object-cover cursor-pointer relative  "
-                alt=""
-              />
-            </div>
-            <div className="picture  rounded-xl relative">
-              <img
-                onClick={handleSmallImageClick}
-                src={shoes4}
-                className="rounded-xl h-[6.25rem] md:h-[5rem] w-full object-cover cursor-pointer relative  "
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-center md:justify-start h-full mb-[6.25rem]">
-            <h5 className="text-[#d7691c] uppercase font-semibold text-[1rem] mb-[1.25rem]">
-              sneaker company
-            </h5>
-            <h2 className="text-[#000] text-[1.875rem] md:text-[1.3rem] uppercase font-bold w-[60%] pb-[1.25rem]">
-              fall limited edition sneakers
-            </h2>
-            <p className="text-[0.813rem] w-[70%] pb-[1.25rem]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-              saepe sed ducimus iste quae cupiditate repellat perferendis
-              dolorum. Quod libero molestiae officia voluptate sapiente beatae,
-              rpellendus tenetur quam, facilis ut inventore ipsam ad natus
-              maxime, magni reiciendis sed. Voluptatibus, quod?
-            </p>
-
-            <p className="flex items-center gap-[0.625rem]">
-              <a className="text-[#000] text-[1.563rem] font-bold">$125 </a>
-              <span className="bg-[#fff2e7] text-[#d7691c] font-bold p-[0.188rem] rounded-[0.313rem]">
-                50 %
-              </span>
-            </p>
-            <p className="text-[#c0c0c8] font-semibold line-through">$250</p>
-            <div className="flex items-center mt-[0.625rem] gap-[1.25rem]">
-              <div className="flex items-center gap-[1.25rem] bg-[#f7f8fd] p-[0.625rem] rounded-md ">
-                <FaMinus
-                  onClick={handleDencremet}
-                  className="text-[#d7691c] font-bold cursor-pointer"
+      <div className="">
+        {basket &&
+          basket?.map((item, i) => (
+            <div key={i} className="grid grid-cols-2 gap-4 px-[9.375rem] xl:px-[3.125rem] lg:px-[1.25rem] md:grid-cols-1">
+              <div key={i} className=" px-[5rem] md:px-[0rem] py-[1.25rem] lg:px-[1.25rem]">
+                <img
+                  ref={bigSrc}
+                  className="h-[28.125rem] md:h-[25rem] w-full object-cover rounded-xl mb-[1.25rem]"
+                  src={bigImgSrc ? bigImgSrc : item.src}
+                  alt=""
                 />
-                <span>{count}</span>
-                <BiPlusMedical
-                  onClick={hanleIncremet}
-                  className="text-[#d7691c] font-semibold cursor-pointer"
-                />
+                <div className="grid grid-cols-4 gap-4 images">
+                  {item?.pictures?.map((cur, i) => (
+                    <div className="picture rounded-xl relative" key={i}>
+                      <img
+                        onClick={handleSmallImageClick}
+                        ref={smallSrc}
+                        src={cur?.src}
+                        className="rounded-xl h-[6.25rem] md:h-[5rem] w-full object-cover cursor-pointer relative  "
+                        alt=""
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
-              <button className="bg-[#ff7d1b] flex items-center gap-[0.625rem] text-white text-[1.25rem] px-[2.5rem] py-[0.313rem] rounded-md transition-all hover:bg-[#ffac6a]">
-                <SlBasket />
-                Add to cart
-              </button>
+              <div className=" ">
+                <div className="flex flex-col justify-center md:justify-start h-full mb-[6.25rem] gap-[1.5rem]">
+                  <h5 className="text-[#d7691c] uppercase font-semibold text-[1rem] ">
+                    {item?.company}
+                  </h5>
+                  <h2 className="text-[#000] text-[1.875rem] md:text-[1.3rem] uppercase font-bold w-[60%] ">
+                    {item?.name}
+                  </h2>
+                  <p className="text-[0.813rem] w-[70%] ">
+                    {item?.title}
+                  </p>
+
+                  <p className="flex items-center gap-[0.625rem]">
+                    <a className="text-[#000] text-[1.563rem] font-bold">
+                      $ {item?.price}
+                    </a>
+                    <span className="bg-[#fff2e7] text-[#d7691c] font-bold p-[0.188rem] rounded-[0.313rem]">
+                      {item?.discount} %
+                    </span>
+                  </p>
+                  <p className="text-[#c0c0c8] font-semibold line-through">
+                    $ {item?.oldprice}
+                  </p>
+                  <div className="flex items-center  gap-[1.25rem]">
+                    <div className="flex items-center gap-[1.25rem] bg-[#f7f8fd] p-[0.625rem] rounded-md ">
+                      <FaMinus
+                        onClick={handleDencremet}
+                        className="text-[#d7691c] font-bold cursor-pointer"
+                      />
+                      <span>{count}</span>
+                      <BiPlusMedical
+                        onClick={hanleIncremet}
+                        className="text-[#d7691c] font-semibold cursor-pointer"
+                      />
+                    </div>
+                    <button className="bg-[#ff7d1b] flex items-center gap-[0.625rem] text-white text-[1.25rem] px-[2.5rem] py-[0.313rem] rounded-md transition-all hover:bg-[#ffac6a]">
+                      <SlBasket />
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </>
   );
--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -18,11 +18,11 @@ export const basket = [
         src: shoes1,
         pictures: [{
                 id: 1,
-                src: shoes1
+                src: shoes2
             },
             {
                 id: 2,
-                src: shoes2
+                src: shoes1
             },
             {
                 id: 3,
@@ -34,86 +34,6 @@ export const basket = [
             },
         ]
     },
-    {
-        id: 2,
-        company: "sneaker company",
-        name: "Shoes 2",
-        price: 150,
-        oldprice: 220,
-        discount: 50,
-        title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam odit eos nihil veniam maxime corporis, quisquam quis doloribus modi optio!",
-        src: shoes1,
-        pictures: [{
-                id: 1,
-                src: shoes1
-            },
-            {
-                id: 2,
-                src: shoes2
-            },
-            {
-                id: 3,
-                src: shoes3
-            },
-            {
-                id: 4,
-                src: shoes4
-            },
-        ]
-    },
-    {
-        id: 3,
-        company: "sneaker company",
-        name: "Shoes 3",
-        price: 190,
-        oldprice: 260,
-        discount: 50,
-        title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam odit eos nihil veniam maxime corporis, quisquam quis doloribus modi optio!",
-        src: shoes1,
-        pictures: [{
-                id: 1,
-                src: shoes1
-            },
-            {
-                id: 2,
-                src: shoes2
-            },
-            {
-                id: 3,
-                src: shoes3
-            },
-            {
-                id: 4,
-                src: shoes4
-            },
-        ]
-    },
-    {
-        id: 4,
-        company: "sneaker company",
-        name: "Shoes 4",
-        price: 160,
-        oldprice: 250,
-        discount: 50,
-        title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam odit eos nihil veniam maxime corporis, quisquam quis doloribus modi optio!",
-        src: shoes1,
-        pictures: [{
-                id: 1,
-                src: shoes1
-            },
-            {
-                id: 2,
-                src: shoes2
-            },
-            {
-                id: 3,
-                src: shoes3
-            },
-            {
-                id: 4,
-                src: shoes4
-            },
-        ]
-    }
+   
 
 ]

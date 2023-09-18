f5c35d0533caa8fd3ac1a9d29d6de65c00088d7f
thejavidev <javidation.l@gmail.com>
2023-09-18 20:31:50 +0400

    v7

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
         ref={overlay}
         className="fixed top-0 left-0 right-0 w-full h-full bgColor  opacity-0 invisible transition-all z-[800]"
       ></div>
-      <header className=" px-[9.375rem] xl:px-[7rem] md:px-[1rem] md:py-[.6rem] md:z-[700] py-[1.25rem]  w-full fixed top-0 left-0 right-0 bg-[#fff]">
+      <header className=" px-[9.375rem] xl:px-[7rem] md:px-[1rem] md:py-[.6rem] md:z-[700] py-[1.25rem]  w-full absolute top-0 left-0 right-0 bg-[#fff]">
         <div className="flex items-center w-full  justify-between  border-b-2 pb-[1.875rem] md:pb-0">
           <div className="flex items-center">
             <div className="mr-[1.25rem] flex items-center gap-[20px]">
